600d3203b075e0feed970c722f240a8827e8e471
Navin Kumar <navin1551@gmail.com>
2020-09-19 15:04:12 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const selectItems = ['item1', 'item2', 'item3']
+
+  const style = {
+    border: '1px solid black',
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: '20px'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Select Filter</h1>
+      <div style={{ marginBottom: '50px' }}>
+        <select>
+          <option>No Filter</option>
+          {selectItems.map(item => {
+            return <option value={item}>{item}</option>
+          })}
+        </select>
+      </div>
+
+      <div style={style}>
+        <span>item1</span>
+        <span>item1</span>
+        <span>item1</span>
+        <span>item1</span>
+      </div>
+
+      <div style={style}>
+        <span>item2</span>
+        <span>item2</span>
+        <span>item2</span>
+        <span>item2</span>
+      </div>
+
+      <div style={style}>
+        <span>item3</span>
+        <span>item3</span>
+        <span>item3</span>
+        <span>item3</span>
+      </div>
     </div>
   );
 }
 
 export default App;
+
